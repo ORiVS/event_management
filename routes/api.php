2577974b25ef435c55848870bb9bb62b64a7ed96
ORiVS <107984700+ORiVS@users.noreply.github.com>
2025-02-03 11:46:54 +0100

    feat(auth): add user logout with email and password

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,4 @@ Route::apiResource('events', EventController::class);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
